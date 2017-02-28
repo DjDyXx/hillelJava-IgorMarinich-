3926e33c4611f55d6578b39ff62a5d5a4d511d08
DjDyXx <djdyxxd1@gmail.com>
2017-03-01 00:18:39 +0200

    ch

--- Lesson6/AnimalsExtends/src/Main.java
+++ Lesson6/AnimalsExtends/src/Main.java
@@ -4,6 +4,9 @@
 public class Main {
     public static void main(String[] args) {
         GuideDog sharik = new GuideDog(1,10,35, "Black", "Sharik",true,true);
+        Fish fish = new Fish(2,10,1,"red","Fish",false);
+        Dog dog = new Dog(2,10,1,"red","Dog",false);
+        Hamster hamster = new Hamster(2,10,1,"red","hamster",false,true);
         System.out.println(sharik.voice());
     }
 }
---
+++ Lesson7/MyCollection/src/MyCollection.java
@@ -0,0 +1,96 @@
+/**
+ * Created by DyXx on 28.02.2017.
+ */
+public class MyCollection {
+    private Object[] array;
+
+    public MyCollection(int size) {
+        this.array = new Object[size];
+    }
+
+    public int size() {
+        return this.array.length;
+    }
+
+    public boolean isEmpty() {
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean contains(Object object) {
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].equals(object)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(Object object) {
+        Object[] newArray = new Object[array.length + 1];
+        for (int i = 0; i < this.array.length; i++) {
+            newArray[i] = this.array[i];
+        }
+        newArray[this.array.length] = object;
+        this.array = newArray;
+    }
+
+    public void remove(Object object) {
+        Object[] newArray = new Object[array.length - 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (object == this.array[i]) {
+                for (; i < newArray.length; i++) {
+                    newArray[i] = this.array[i + 1];
+                }
+            } else {
+                newArray[i] = this.array[i];
+            }
+        }
+        this.array = newArray;
+    }
+
+    public void addAll(MyCollection collection) {
+        Object[] newArray = new Object[this.array.length + collection.array.length];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = this.array[i];
+        }
+        for (int i = 0; i < collection.array.length; i++) {
+            newArray[this.array.length - 1 + i] = collection.array[i];
+        }
+        this.array = newArray;
+    }
+
+
+    public void clear() {
+        this.array = new Object[0];
+    }
+
+    public void retainAll(MyCollection collection) {
+        for (int i = 0; i < this.array.length; i++) {
+            if (!collection.contains(this.array[i])) {
+                this.remove(this.array[i]);
+            }
+        }
+    }
+
+    public void removeAll(MyCollection collection) {
+        MyCollection removedCollection = new MyCollection(collection.size());
+    }
+
+    public boolean containsAll(MyCollection collection) {
+        int counter = 0;
+        for (int i = 0; i < collection.array.length; i++) {
+            for (int j = 0; j < this.array.length; j++) {
+                if (collection.array[i] == this.array[j]) {
+                    counter++;
+                }
+            }
+        }
+        return counter == collection.size();
+    }
+
+}
